e967f8fd0b16fef94d257577bafc903c5167b2a8
Ken Chase <kenchase@kenchase.com>
2026-09-25 10:53:58 -0400

    Updates

--- workers/fetch-esri-worker-script.worker.js
+++ workers/fetch-esri-worker-script.worker.js
@@ -1,4 +1,4 @@
-/*! Package:fetch-esri-worker-script: 2.3.0 - "ddb0318539097de7d66a337583664aee3ffea0fe" - 2026-09-25T13:52:03.193Z */(()=>{"use strict";
+/*! Package:fetch-esri-worker-script: 2.3.0 - "21ac955445997d3475cf3435a23f75a9704c4687" - 2026-09-25T14:46:21.213Z */(()=>{"use strict";
 /**
  * @license
  * Copyright 2019 Google LLC
